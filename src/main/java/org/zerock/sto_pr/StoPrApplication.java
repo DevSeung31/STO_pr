82a82c21dec5dc336bd247422663404e4ce7181a
lsh <lleesh36@gmail.com>
2026-04-03 09:47:41 +0900

    git init 완료

---
+++ src/main/java/org/zerock/sto_pr/StoPrApplication.java
@@ -0,0 +1,13 @@
+package org.zerock.sto_pr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoPrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoPrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/zerock/sto_pr/StoPrApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerock.sto_pr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoPrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
